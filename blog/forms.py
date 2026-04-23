ebb77ab479c5be2fb2d9ae3619fbbb32e96cf333
白 文斌 <haku@SEKKEICAD.local>
2026-04-23 11:45:41 +0900

    修改分类标签之前

--- blog/forms.py
+++ blog/forms.py
@@ -1,12 +1,23 @@
 from django import forms
 from .models import Comment
 
+
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        # 只需要让用户输入昵称和内容，文章关联由后台自动处理
         fields = ['author_name', 'body']
+        labels = {
+            'author_name': '你的名字',
+            'body': '评论内容',
+        }
         widgets = {
-            'author_name': forms.TextInput(attrs={'placeholder': '您的昵称', 'class': 'form-control'}),
-            'body': forms.Textarea(attrs={'placeholder': '写下您的评论...', 'class': 'form-control', 'rows': 4}),
+            'author_name': forms.TextInput(attrs={
+                'class': 'input-field',
+                'placeholder': '请输入你的名字'
+            }),
+            'body': forms.Textarea(attrs={
+                'class': 'textarea-field',
+                'placeholder': '写下你的想法…',
+                'rows': 5
+            }),
         }
